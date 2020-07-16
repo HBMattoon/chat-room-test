4ac2dede4630a46935ab55a42d0efb7e24b7fedc
Henry Mattoon <hmattoon@mail.sfsu.edu>
2020-07-15 20:52:43 -0700

    create classes for room and user

---
+++ server/chatroom/user.js
@@ -0,0 +1,13 @@
+
+// User class, will have user name, unique ID, and array of all the conversations/rooms they are currently active in.
+class User {
+  constructor (name, id) {
+    this.name = name,
+    this.id = id,
+    this.conversations = [];
+  }
+
+
+
+
+}
